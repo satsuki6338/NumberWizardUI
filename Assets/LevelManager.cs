ee8483d7dcc41307a2be6dca8ac4388bab3311ec
satsuki6338 <vuchithien95@yahoo.com.vn>
2016-09-19 22:14:10 -0700

    First Commit
    First Commit with basic program functions

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level Requested: " + name);
+        Application.LoadLevel(name);
+    }
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Requested");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberWizards.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    public Text guessText;
+
+    public int maxGuessesAllowed = 5;
+
+	// Use this for initialization
+	void Start () {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = (max + min) / 2;
+      
+        max = max + 1;
+    }
+
+    //run when need to update the guess
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0)
+        {
+            Application.LoadLevel("Lose");
+        }
+        
+    }
+    
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
